db2f32f6506ead71111c09c27d5916cbfec19ad2
micbuffa <micbuffa@gmail.com>
2023-01-16 18:13:22 +0100

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { RenduDirective } from './shared/rendu.directive'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent,
+    RenduDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatIconModule, MatDividerModule,
+    MatInputModule, MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css'],
+})
+export class AssignmentsComponent {
+  titre = 'Liste des Assignments';
+  assignments = [
+    {
+      nom:"Devoir Angular de Mr Buffa",
+      dateDeRendu:"2023-01-26",
+      rendu : false
+    },
+    {
+      nom:"Devoir R de Mr Pasquier",
+      dateDeRendu:"2023-02-15",
+      rendu : false
+    },
+    {
+      nom:"Devoir Grails de Mr galli",
+      dateDeRendu:"2022-12-16",
+      rendu : true
+    }
+  ];
+
+  getColor(a:any) {
+    if (a.rendu) {
+      return 'green';
+    } else {
+      return 'red';
+    }
+  }
+}
